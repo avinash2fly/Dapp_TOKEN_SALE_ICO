fac5c3db6a4617db06ddb5a2313261e8a5f9d063
Avinash Gupta <avinash2fly@gmail.com>
2018-04-22 21:29:55 +1000

    transfer tokens done

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -3,13 +3,44 @@ pragma solidity ^0.4.2;
 //ERC-20 standard
 
 contract DappToken{
+  //Name
+  string public name = "DApp Token";
+  //Symbol
+  string public symbol ="DAPP";
+
+  // below is not part of ERC-20 standard
+  string public standard ="DApp Token v1.0";
+
   // Constructor
   // Set the total number of tokens
   //Read the total number of tokens
   uint256 public totalSupply;
 
-  function DappToken() public{
-    totalSupply = 1000000;
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+    );
+  mapping(address => uint256) public balanceOf;
+  //
+  function DappToken(uint256 _initialSupply) public{
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
+    //allocate the intial supply
+  }
+
+  //tranfer
+
+  function tranfer(address _to, uint256 _value) public returns (bool success){
+    //Exception if account doesn't have enough
+    require(balanceOf[msg.sender] >= _value);
+    //Transfer the balanceOf
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    //Transfer Event
+    Transfer(msg.sender,_to,_value);
+    //Return a boolean
+    return true;
 
   }
 }
