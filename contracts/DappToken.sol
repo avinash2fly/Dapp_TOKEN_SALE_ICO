5640ed18f167da7930600051ef20d804c1bb4cb6
Avinash Gupta <avinash2fly@gmail.com>
2018-04-24 18:48:21 +1000

    added transfer delegater fucntion

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -21,8 +21,21 @@ contract DappToken{
     address indexed _to,
     uint256 _value
     );
+
+    //Approve
+    event Approval(
+      address indexed _owner,
+      address indexed _spender,
+      uint256 _value
+      );
+
   mapping(address => uint256) public balanceOf;
-  //
+
+
+  //allowance
+  mapping(address => mapping(address => uint256)) public allowance;
+
+
   function DappToken(uint256 _initialSupply) public{
     balanceOf[msg.sender] = _initialSupply;
     totalSupply = _initialSupply;
@@ -31,7 +44,7 @@ contract DappToken{
 
   //tranfer
 
-  function tranfer(address _to, uint256 _value) public returns (bool success){
+  function transfer(address _to, uint256 _value) public returns (bool success){
     //Exception if account doesn't have enough
     require(balanceOf[msg.sender] >= _value);
     //Transfer the balanceOf
@@ -41,6 +54,42 @@ contract DappToken{
     Transfer(msg.sender,_to,_value);
     //Return a boolean
     return true;
-
   }
+
+  // Delegated Transfer
+
+  //approve
+  function approve(address _spender,uint256 _value) public returns(bool success){
+    // allownace
+    allowance[msg.sender][_spender] = _value;
+    //Approve event
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  //tranferFrom
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+    //Require _from has enough tokens
+    require(_value <= balanceOf[_from]);
+
+    //Require allowance is big enough
+    require(_value <= allowance[_from][msg.sender]);
+
+    // change the balanceOf
+    balanceOf[_from] -= _value;
+    balanceOf[_to]+= _value;
+
+    // update the allowance
+    allowance[_from][msg.sender]-=_value;
+
+    // transfer event
+    Transfer(_from,_to,_value);
+
+    // return a boolean
+    return true;
+  }
+
+
+
+
 }
